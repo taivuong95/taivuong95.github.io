d0a6fc4f7e6f70f99eb67068651936aaea9d483d
TaiVuong <taivt95@gmail.com>
2019-05-01 17:12:44 +0700

    add comment fb

--- 1.js
+++ 1.js
@@ -5,5 +5,9 @@
     $('.xuong,.kn').click(function(){
         $('body, html').animate({scrollTop:vitricuaedu}, 1500);
     })
+
+    $('.cm').click(function(){
+        $('body, html').animate({scrollTop:$('#phanhoi').offset().top}, 1500);
+    })
 })  
  
